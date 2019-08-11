f09677ad6edad6f36f9ecde4ad008ac8740a0512
DovgaNik <nikita.dovgan.05@hotmail.com>
2019-08-11 16:42:31 +0300

    Fixed usage part of comments Replaced file_print ==> filePrint

--- src/filePrint.java
+++ src/filePrint.java
@@ -6,8 +6,8 @@
 This class prints text from file 
 To read from file you must type text in print function arguments. For example:
 
-import File_operatons.file_print;
-file_print printer = new file_print();
+import File_operatons.filePrint;
+filePrint printer = new filePrint();
 printer.print("file_name");
 
 If the printing from the file was succesfull function will return 0, if there is printer error

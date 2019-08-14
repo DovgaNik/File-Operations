534c2f49cd3103c589a0b14f2ee4690f3efe75c0
DovgaNik <nikita.dovgan.05@hotmail.com>
2019-08-14 18:56:23 +0300

    Written documantation for the new class

--- src/fileOperations/simple/folderExists.java
+++ src/fileOperations/simple/folderExists.java
@@ -2,6 +2,15 @@
 //*DovgaNik 2018-2019* 
 //********************
 
+/*
+This class gives you information about the existant of the folder
+
+Usage:
+    import fileOperations.simple.folderExists;
+    folderExists checker = new folderExists;
+    lister.check(folderName);
+*/
+
 package fileOperations.simple;
 
 import java.io.File;

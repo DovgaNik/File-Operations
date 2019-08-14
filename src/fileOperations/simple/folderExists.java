3b83aa73a2c92a041150feb028becf2bc34e97b5
DovgaNik <nikita.dovgan.05@hotmail.com>
2019-08-14 17:39:59 +0300

    Written folder check class

--- src/fileOperations/simple/folderExists.java
+++ src/fileOperations/simple/folderExists.java
@@ -4,6 +4,15 @@
 
 package fileOperations.simple;
 
+import java.io.File;
+
 public class folderExists {
-    
+    int check(String folderName){
+        File tmpDir = new File("/var/tmp");
+        if(tmpDir.exists())
+            if (tmpDir.isDirectory()){
+                return 0;
+            }
+        return 1;
+    }
 }
